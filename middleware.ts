4912990629bd9b51cdaf260b2ce875b75334dc26
Alan Williams <alan.williams@vercel.com>
2023-04-18 15:20:10 +0100

    check bypass and set cookie

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-	matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)"
+	matcher: "/((?!api|_next/static|_next/image|favicon.ico|404).*)"
 }
 
 export function middleware(request: NextRequest) {
-	console.log(request);
-	return NextResponse.next();
+	if (request.headers['x-vercel-protection-bypass'] === process.env.SECRET_BYPASS) {
+		const response = NextResponse.next();
+		response.cookies.set('x-vercel-set-bypass-cookie', true);
+		return response;
+	}
+	return NextResponse.rewrite(request.nextUrl.href.replace('editor', '404'));
 }
