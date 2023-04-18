a96dfc12b2de593d29497ea65342971042dc903e
Alan Williams <alan.williams@vercel.com>
2023-04-18 14:12:43 +0100

    no restriction in page

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const config = {
+	matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)"
+}
+
+export function middleware(request: NextRequest) {
+	console.log(request);
+	return NextResponse.next();
+}
---
+++ pages/editor.tsx
@@ -0,0 +1,7 @@
+export default function Editor() {
+	return (
+		<main>
+			<h1>RESTRICTED EDITOR</h1>
+		</main>
+	)
+}
